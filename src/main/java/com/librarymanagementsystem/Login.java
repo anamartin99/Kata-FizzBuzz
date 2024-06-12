f9493ce9d400bd5b6f9813fdd2cffc3174f1f35f
JMPolvillo <rothkildam@gmail.com>
2024-06-12 12:31:34 +0200

    Added Login code

--- src/main/java/com/librarymanagementsystem/Login.java
+++ src/main/java/com/librarymanagementsystem/Login.java
@@ -2,7 +2,7 @@ package com.librarymanagementsystem;
 
 import java.util.Scanner;
 
-public class LoginMain {
+public class Login {
 
     public static void main(String[] args) {
         String Username = "user";
